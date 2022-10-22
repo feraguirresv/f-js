5fbb42a00881f6079784fd5f3d1415c12c3abf05
Fernando Aguirre <feraguirre989898@gmail.com>
2022-10-22 00:01:26 -0300

    Modifique

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
 botonVaciar.addEventListener('click', () => {
                                 
     
-    if (carrito.length >= 0 ) { Swal.fire({
+    if (carrito.length >= 1 ) { Swal.fire({
         title: 'Estas seguro de eliminar todos los productos?',
         icon: 'warning',
         showCancelButton: true,
@@ -40,8 +40,7 @@ botonVaciar.addEventListener('click', () => {
           )
           carrito.length = 0
           local.StorageClear(); 
-        
-          actualizarCarrito()
+          actualizarCarrito();
         }
       })} else{
         Swal.fire({                                             
